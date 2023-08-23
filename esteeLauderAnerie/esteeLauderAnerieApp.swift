76802dd871a9a92fc94c5d1affdcac9e3a2375d7
Anerie Patel <aneriep@gmail.com>
2023-08-22 22:14:17 -0400

    Initial Commit

---
+++ esteeLauderAnerie/esteeLauderAnerieApp.swift
@@ -0,0 +1,17 @@
+//
+//  esteeLauderAnerieApp.swift
+//  esteeLauderAnerie
+//
+//  Created by Anerie Patel on 2023-08-22.
+//
+
+import SwiftUI
+
+@main
+struct esteeLauderAnerieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
