ebbda70e4c8defc5b944cc3e69e1f4e8bccadf0b
Anerie Patel <aneriep@gmail.com>
2023-08-22 22:37:20 -0400

    more

--- esteeLauderAnerie/ContentView.swift
+++ esteeLauderAnerie/ContentView.swift
@@ -9,13 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Estee Lauder")
+        
+        NavigationStack{
+            ZStack {
+                Color(red: 0.973, green: 0.744, blue: 0.801)
+                    .ignoresSafeArea()
+                    .overlay(
+                
+                VStack {
+                      
+                    Text("Estee Lauder's New Nutritous Collection")
+                        .font(.title)
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color(red: 0.845, green: 0.12, blue: 0.208))
+                        .multilineTextAlignment(.center)
+                    
+                    Image("backgroundMain")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        
+                   
+                       
+                }
+                .padding()
+                )
+            }
+                
         }
-        .padding()
+       
     }
 }
 
