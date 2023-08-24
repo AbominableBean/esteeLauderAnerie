b4cf3ff967e903e7d355c62055eae24b38c43dd4
Anerie Patel <aneriep@gmail.com>
2023-08-24 18:38:02 -0400

    fixed visuals

--- esteeLauderAnerie/mainMenu.swift
+++ esteeLauderAnerie/mainMenu.swift
@@ -9,13 +9,15 @@ import SwiftUI
 
 struct mainMenu: View {
     var body: some View {
+        
+        //add less words
         ZStack {
             Color(red: 0.907, green: 0.902, blue: 0.798)
                 .ignoresSafeArea()
                 .overlay(
             
                     VStack {
-                        Text("Welcome to Estée Lauder's new Nutritous line!")
+                        Text("Welcome to Estée Lauder's New Nutritous line!")
                             .font(.custom("Optima-Regular", size: 30))
                             .multilineTextAlignment(.center)
                             .foregroundColor(Color(red: 0.539, green: 0.079, blue: 0.28))
@@ -23,11 +25,11 @@ struct mainMenu: View {
                         
                         HStack {
                             
-                            Text("Go to our skin quiz to get matched to Estée Lauder's best Nutritous product for you.")
-                                .font(.custom("Optima-Regular", size: 15))
+                            Text("Find the best products for your skin.")
+                                .font(.custom("Optima-Regular", size: 18))
                                 .foregroundColor(Color(hue: 0.227, saturation: 0.921, brightness: 0.388))
                                 .multilineTextAlignment(.center)
-                                .padding(.all)
+                                .padding()
                             
                             NavigationLink(destination: skinQuiz()) {
                                 Image("skinQuizIcon")
@@ -37,16 +39,15 @@ struct mainMenu: View {
                                     .frame(width: 150, height: 150)
                                     .clipped()
                                 
-                            }
-                            
+                            }                            
                         }
                         
                         HStack {
-                            Text("Go to our 'Find the Product' game & play to win discounts.")
-                                .font(.custom("Optima-Regular", size: 15))
+                            Text("Play 'Find the Product' to win discounts.")
+                                .font(.custom("Optima-Regular", size: 18))
                                 .foregroundColor(Color(hue: 0.229, saturation: 0.921, brightness: 0.388))
                                 .multilineTextAlignment(.center)
-                                .padding(.all)
+                                .padding()
                             
                             NavigationLink(destination: productGame()) {
                                 Text("Insert image here")
@@ -54,7 +55,8 @@ struct mainMenu: View {
                             }
                             
                         }
-                        Text("Learn more about Estee Lauder's new Nutritution line products.")
+                        .padding(.horizontal)
+                        Text("Learn more about Estée Lauder's new Nutritution line products.")
                             .font(.custom("Optima-Regular", size: 20))
                             .multilineTextAlignment(.center)
                             .foregroundColor(Color(red: 0.539, green: 0.079, blue: 0.28))
@@ -71,36 +73,38 @@ struct mainMenu: View {
                             NavigationLink(destination: radiantLotion()) {
                                 Image("radiantLotion")
                                     .resizable()
-                                    .padding(.all, -2.0)
+                                    .padding(.all, -1)
                                     .scaledToFit()
-                                    .frame(width: 80, height: 80)
+                                    .frame(width: 100, height: 100)
                                     .clipped()
                                 
                             }
                             NavigationLink(destination: meltingSoft()) {
                                 Image("meltingSoft")
                                     .resizable()
-                                    .padding(.all, -2.0)
+                                    .padding(.all, -1)
                                     .scaledToFit()
-                                    .frame(width: 80, height: 80)
+                                    .frame(width: 100, height: 100)
                                     .clipped()
                             }
+                        }
+                        HStack {
                             
                             NavigationLink(destination: airyLotion()) {
                                 Image("airyLotion")
                                     .resizable()
-                                    .padding(.all, -2.0)
+                                    .padding(.all, -1)
                                     .scaledToFit()
-                                    .frame(width: 80, height: 80)
+                                    .frame(width: 100, height: 100)
                                     .clipped()
                             }
                             
                             NavigationLink(destination: foamCleanser()) {
                                 Image("foamCleanser")
                                     .resizable()
-                                    .padding(.all, -2.0)
+                                    .padding(.all, -1)
                                     .scaledToFit()
-                                    .frame(width: 80, height: 80)
+                                    .frame(width: 100, height: 100)
                                     .clipped()
                             }
                             
