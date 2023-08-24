5a7435fb046ccc768284e86e31d82dbdde8b4f60
Anerie Patel <aneriep@gmail.com>
2023-08-24 16:32:40 -0400

    progress

--- esteeLauderAnerie/mainMenu.swift
+++ esteeLauderAnerie/mainMenu.swift
@@ -14,17 +14,41 @@ struct mainMenu: View {
                 .ignoresSafeArea()
                 .overlay(
             
-            VStack {
-                Text("Welcome to Estee Lauder's new Nutritous line!")
-                    .font(.custom("Optima-Regular", size: 30))
-                    .multilineTextAlignment(.center)
-                    .foregroundColor(Color(hue: 0.233, saturation: 0.949, brightness: 0.469))
+                    VStack {
+                        Text("Welcome to Estee Lauder's new Nutritous line!")
+                            .font(.custom("Optima-Regular", size: 30))
+                            .multilineTextAlignment(.center)
+                            .foregroundColor(Color(red: 0.653, green: 0.117, blue: 0.169))
+                        
+                        
+                        HStack {
+                            
+                                Text("Click the image to complete a skin quiz to find the best Nutritious product for you")
+                                    .font(.custom("Optima-Regular", size: 15))
+                                    .foregroundColor(Color(hue: 0.229, saturation: 0.921, brightness: 0.388))
+                                    .multilineTextAlignment(.center)
+                                    .padding(.all)
+                                NavigationLink(destination: skinQuiz()) {
+                                    
+                                    
+                                    Image("skinQuizIcon")
+                                        .resizable()
+                                        .padding(.all)
+                                        .scaledToFit()
+                                        .frame(width: 150, height: 150)
+                                        .clipped()
+                                    
+                                
+                            }
+                        }
+                    }
+                    )
             }
             
-            )
+            
         }
     }
-}
+
 
 struct mainMenu_Previews: PreviewProvider {
     static var previews: some View {
---
+++ esteeLauderAnerie/skinQuiz.swift
@@ -0,0 +1,20 @@
+//
+//  skinQuiz.swift
+//  esteeLauderAnerie
+//
+//  Created by Anerie Patel on 2023-08-24.
+//
+
+import SwiftUI
+
+struct skinQuiz: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct skinQuiz_Previews: PreviewProvider {
+    static var previews: some View {
+        skinQuiz()
+    }
+}
