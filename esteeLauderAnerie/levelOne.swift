ab820a4be81d09cef99d65bb97d9ec6f0730cf74
Anerie <141164361+anerie-anerie@users.noreply.github.com>
2023-08-25 17:45:32 -0400

    Add files via upload

--- esteeLauderAnerie/levelOne.swift
+++ esteeLauderAnerie/levelOne.swift
@@ -9,14 +9,24 @@ import SwiftUI
 
 struct levelOne: View {
     var body: some View {
-        VStack {
-            Text("Welcome to Level 1")
-            Image("esteeGame1")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .padding()
-            
-            //add zoom in feature and click counter
+        
+        ZStack {
+            Color(hue: 0.96, saturation: 0.238, brightness: 1.0)
+                .ignoresSafeArea()
+                .overlay(
+            VStack {
+                Text("Welcome to Level 1")
+                    .font(.custom("Optima-Regular", size: 30))
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(Color(red: 0.539, green: 0.079, blue: 0.28))
+                Image("esteeGame1")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding()
+                
+                //add zoom in feature and click counter
+            }
+            )
         }
     }
 }
